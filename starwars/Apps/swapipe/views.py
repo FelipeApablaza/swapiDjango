9b8a40b74cb905e75333af601a13dfb2735b345a
PIPEAPABLAZA <feapablaza@uc.cl>
2019-03-24 15:46:43 -0300

    asa

--- starwars/Apps/swapipe/views.py
+++ starwars/Apps/swapipe/views.py
@@ -12,10 +12,11 @@ def homeView(request):
     num= r['count']
     res= r['results']
 
+    roman={1:'I',2:'II',3:'III',4:'IV',5:'V',6:'VI',7:'VII'}
     dicc={'films':[]}
     for i in range (num):
-        dicc['films'].append({'title':res[i]['title'],'date':res[i]['release_date'],'director':res[i]['director']
-        ,'producer':res[i]['producer'],'episode':res[i]['episode_id']})
+        dicc['films'].append({'title':res[i]['title'],'date':res[i]['release_date'][0:4],'director':res[i]['director']
+        ,'producer':res[i]['producer'],'episode':roman[res[i]['episode_id']]})
 
     dicc['films'].sort(key=lambda x:x['episode'])
     return render(request,'home.html',dicc)
